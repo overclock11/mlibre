10457bb4fd125d7c0e8c25bcab1cf31531ad33eb
Julian Borray <julian.borray@bizagi.com>
2021-05-29 14:34:56 -0500

    add search bar and welcome components

---
+++ src/common/SearchBar/SearchBar.tsx
@@ -0,0 +1,33 @@
+import {Link} from "react-router-dom";
+import {useHistory} from "react-router-dom";
+import {useLocation} from "react-router-dom";
+import SearchIcon from '@material-ui/icons/Search';
+
+export const SearchBar = () => {
+    const history = useHistory();
+    //const location: string = useLocation().pathname;
+    const searchKeyWord = (event: any, origin = 'input') => {
+        if (event.charCode === 13 || origin === 'button') {
+            history.push(`/items?search=${(document.getElementById("inputSearchBar") as HTMLInputElement).value}`);
+            (document.getElementById("inputSearchBar") as HTMLInputElement).value = '';
+        }
+    }
+    return (
+        <div className="search">
+            <Link to="/">
+                <img src="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.14.1/mercadolibre/logo__small.png"
+                     alt="logo"
+                     className="search__logo"
+                />
+            </Link>
+            <input type="search"
+                   className="search__input-search"
+                   id="inputSearchBar"
+                   placeholder="Buscar productos, marcas y más..."
+                   onKeyPress={searchKeyWord}/>
+            <button className="search__button-search" onClick={e => searchKeyWord(e, 'button')}>
+                <SearchIcon fontSize="small"/>
+            </button>
+        </div>
+    )
+}
---
+++ src/components/welcome/Welcome.tsx
@@ -0,0 +1,11 @@
+import './Welcome.scss';
+
+export const Welcome = () =>{
+    return (
+        <main className="home">
+            <h1 className="home__main-text">Mercado Libre</h1>
+            <h3 className="home__main-text">Donde puedes comprar y vender miles de productos en toda Latinoamerica, entre electronicos, informatica, celulares, camaras digitales,
+                vehiculos, libros, ropa y mucho mas.</h3>
+        </main>
+    );
+}
