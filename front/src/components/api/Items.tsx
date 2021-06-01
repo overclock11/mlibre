b9647669ecd3a4b77ae26ee873f557f0039f141b
Julian Borray <julian.borray@bizagi.com>
2021-06-01 18:34:12 -0500

    ajustar api

--- front/src/components/api/Items.tsx
+++ front/src/components/api/Items.tsx
@@ -1,8 +1,8 @@
 const SearchByKeyWord = async (keyWord: string) => {
-    return fetch(`http://192.168.0.2:3007/api/items/?q=${keyWord}`,{cache: 'force-cache'});
+    return fetch(`api/items/?q=${keyWord}`,{cache: 'force-cache'});
 }
 const GetItemById = async (id: string) =>{
-    return fetch(`http://192.168.0.2:3007/api/items/${id}`,{cache: 'force-cache'});
+    return fetch(`api/items/${id}`,{cache: 'force-cache'});
 }
 
 export {SearchByKeyWord, GetItemById};
