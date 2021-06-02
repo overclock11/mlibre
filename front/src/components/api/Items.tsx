30979c3774cfff9c1dd2b0b3812d698770ca8e99
Julian Borray <julian.borray@bizagi.com>
2021-06-01 19:05:20 -0500

    fix api

--- front/src/components/api/Items.tsx
+++ front/src/components/api/Items.tsx
@@ -1,8 +1,8 @@
 const SearchByKeyWord = async (keyWord: string) => {
-    return fetch(`api/items/?q=${keyWord}`,{cache: 'force-cache'});
+    return fetch(`/api/items/?q=${keyWord}`,{cache: 'force-cache'});
 }
 const GetItemById = async (id: string) =>{
-    return fetch(`api/items/${id}`,{cache: 'force-cache'});
+    return fetch(`/api/items/${id}`,{cache: 'force-cache'});
 }
 
 export {SearchByKeyWord, GetItemById};
